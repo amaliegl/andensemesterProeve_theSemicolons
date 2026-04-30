c8c79d6dbd3e799e0ca7c5a272fba3ee3d28d847
amali <amaliegp@gmail.com>
2026-04-30 10:38:08 +0200

    Initial commit

---
+++ src/main/java/org/example/andensemesterproeve_thesemicolons/AndensemesterProeveTheSemicolonsApplication.java
@@ -0,0 +1,13 @@
+package org.example.andensemesterproeve_thesemicolons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndensemesterProeveTheSemicolonsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AndensemesterProeveTheSemicolonsApplication.class, args);
+    }
+
+}
