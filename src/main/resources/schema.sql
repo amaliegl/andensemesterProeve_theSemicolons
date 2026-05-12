a32d8be29c57af5938600a45a1d7cc712109e5ff
amali <amaliegp@gmail.com>
2026-05-12 14:47:45 +0200

    sql updated

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,15 +3,15 @@ CREATE TABLE users (
                        username VARCHAR(50) NOT NULL,
                        password VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
                        email VARCHAR(50) NOT NULL,
-                       role ENUM('Spiller', 'Arrangoer', 'Admin')
+                       title ENUM('Spiller', 'Arrangoer', 'Admin')
 );
 
 CREATE TABLE cards (
                        id INT AUTO_INCREMENT PRIMARY KEY,
                        name VARCHAR(50) NOT NULL,
-                       type ENUM ('Artifact', 'Artifact Creature', 'Basic Land', 'Creature', 'Enchantment', 'Enchantment Creature', 'Instant', 'Kindred Instant', 'Land', 'Legendary Creature', 'Legendary Enchantment Creature', 'Legendary Planeswalker', 'Snow Creature', 'Sorcery') NOT NULL,
-                       set VARCHAR(3) NOT NULL,
-                       rarity ENUM ('Common', 'Uncommon', 'Rare', 'Mythic Rare', 'Land') NOT NULL,
+                       type ENUM('Artifact', 'Artifact_Creature', 'Basic_Land', 'Creature', 'Enchantment', 'Enchantment_Creature', 'Instant', 'Kindred_Instant', 'Land', 'Legendary_Creature', 'Legendary_Enchantment_Creature', 'Legendary_Planeswalker', 'Snow_Creature', 'Sorcery') NOT NULL,
+                       set_abbreviation VARCHAR(3) NOT NULL,
+                       rarity ENUM('Common', 'Uncommon', 'Rare', 'Mythic Rare', 'Land') NOT NULL,
                        image_url VARCHAR(120) NOT NULL,
                        reference_url VARCHAR(100) NOT NULL
 );
