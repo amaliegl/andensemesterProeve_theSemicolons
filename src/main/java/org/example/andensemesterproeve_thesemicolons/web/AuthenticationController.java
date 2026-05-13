0c77078d1f4a796697b49391682af9d2c2ffd032
sara <saj016@edu.zealand.dk>
2026-05-13 10:59:25 +0200

    Change to PostMapping for login

--- src/main/java/org/example/andensemesterproeve_thesemicolons/web/AuthenticationController.java
+++ src/main/java/org/example/andensemesterproeve_thesemicolons/web/AuthenticationController.java
@@ -1,7 +1,9 @@
 package org.example.andensemesterproeve_thesemicolons.web;
 
 import jakarta.servlet.http.HttpSession;
+import org.example.andensemesterproeve_thesemicolons.application.UserService;
 import org.example.andensemesterproeve_thesemicolons.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AuthenticationController {
+
+    @Autowired
+    private UserService userService;
+
+    public AuthenticationController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/login2")
     public String getLoginPage() {
@@ -24,7 +33,15 @@ public class AuthenticationController {
 
     @PostMapping("/login")
     public String login(@ModelAttribute User user, HttpSession session, Model model) {
-        return "login";
+        User loggedIn = userService.login(user.getUsername(), user.getPassword());
+
+        if (loggedIn != null) {
+            session.setAttribute("currentUser", loggedIn);
+            return "redirect:/home";
+        } else {
+            model.addAttribute("error", "Forkert brugernavn eller adgangskode.");
+            return "login";
+        }
     }
 
     @GetMapping("/register")
