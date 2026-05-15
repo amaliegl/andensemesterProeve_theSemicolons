f936011f786ced730b9b152927be298fde5653a4
amali <amaliegp@gmail.com>
2026-05-15 10:10:46 +0200

    html homepage simple setup

---
+++ src/main/java/org/example/andensemesterproeve_thesemicolons/web/DeckController.java
@@ -0,0 +1,13 @@
+package org.example.andensemesterproeve_thesemicolons.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DeckController {
+
+    @GetMapping("myDecks")
+    public String getMyDeck(){
+        return "/deck/myDecks";
+    }
+}
---
+++ src/main/java/org/example/andensemesterproeve_thesemicolons/web/HomePageController.java
@@ -0,0 +1,14 @@
+package org.example.andensemesterproeve_thesemicolons.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomePageController {
+
+    @GetMapping("/homePage")
+    public String getHomePage(){
+        return "/homepage";
+    }
+
+}
