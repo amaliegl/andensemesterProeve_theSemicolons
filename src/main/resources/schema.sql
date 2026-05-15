4dd40da55b8a803117cc22697a0d9a2dfa43560e
sara <saj016@edu.zealand.dk>
2026-05-15 11:34:09 +0200

    Opdateret schema til users tabellen, så brugernavn og email er unik

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE users (
                        id INT AUTO_INCREMENT PRIMARY KEY,
-                       username VARCHAR(50) NOT NULL,
+                       username VARCHAR(50) UNIQUE NOT NULL,
                        password VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-                       email VARCHAR(50) NOT NULL,
+                       email VARCHAR(50) UNIQUE NOT NULL,
                        title ENUM('Spiller', 'Arrangoer', 'Admin')
 );
 
