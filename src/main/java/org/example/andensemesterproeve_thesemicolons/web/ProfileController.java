9aef62b828a430619a5459bac4c3af0c3398af93
amali <amaliegp@gmail.com>
2026-05-15 16:43:29 +0200

    Profil opdateret med information

--- src/main/java/org/example/andensemesterproeve_thesemicolons/web/ProfileController.java
+++ src/main/java/org/example/andensemesterproeve_thesemicolons/web/ProfileController.java
@@ -1,13 +1,22 @@
 package org.example.andensemesterproeve_thesemicolons.web;
 
+import jakarta.servlet.http.HttpSession;
+import org.example.andensemesterproeve_thesemicolons.domain.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProfileController {
 
     @GetMapping("/myProfile")
-    public String GetMyProfile(){
+    public String GetMyProfile(Model model, HttpSession session){
+        User user = (User) session.getAttribute("currentUser");
+        if (user == null){
+            return "redirect:/login";
+        }
+        model.addAttribute("user", user);
         return "/profile/myProfile";
     }
+
 }
