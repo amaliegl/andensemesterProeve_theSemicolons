143a6154b132435cdd452db76dd2d04496c3a0b1
sara <saj016@edu.zealand.dk>
2026-05-18 20:46:36 +0200

    Funktionalitet tilføjet: Filtrering på kort-sæt og/eller kort-type

--- src/main/java/org/example/andensemesterproeve_thesemicolons/application/CardService.java
+++ src/main/java/org/example/andensemesterproeve_thesemicolons/application/CardService.java
@@ -1,10 +1,13 @@
 package org.example.andensemesterproeve_thesemicolons.application;
 
+import org.example.andensemesterproeve_thesemicolons.domain.Card;
 import org.example.andensemesterproeve_thesemicolons.domain.CardType_ENUM;
+import org.example.andensemesterproeve_thesemicolons.domain.User;
 import org.example.andensemesterproeve_thesemicolons.repository.ICardRepository;
 import org.example.andensemesterproeve_thesemicolons.repository.IUserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -23,5 +26,35 @@ public class CardService {
 
     public List<CardType_ENUM> getAllTypes() {
         return cardRepository.findAllUniqueTypes();
+    }
+
+    public List<Card> filterUserCardsBySet(User user, String set) {
+        List<Card> filteredCards = new ArrayList<>();
+        for (int i = 0; i < user.getCards().size(); i++) {
+            if (user.getCards().get(i).getSet().equals(set)) {
+                filteredCards.add(user.getCards().get(i));
+            }
+        }
+        return filteredCards;
+    }
+
+    public List<Card> filterUserCardsByType(User user, String type) {
+        List<Card> filteredCards = new ArrayList<>();
+        for (int i = 0; i < user.getCards().size(); i++) {
+            if (user.getCards().get(i).getType().name().equals(type)) {
+                filteredCards.add(user.getCards().get(i));
+            }
+        }
+        return filteredCards;
+    }
+
+    public List<Card> filterUserCardsBySetAndType(User user, String set, String type) {
+        List<Card> filteredCards = new ArrayList<>();
+        for (int i = 0; i < user.getCards().size(); i++) {
+            if (user.getCards().get(i).getSet().equals(set) && user.getCards().get(i).getType().name().equals(type)) {
+                filteredCards.add(user.getCards().get(i));
+            }
+        }
+        return filteredCards;
     }
 }
--- src/main/java/org/example/andensemesterproeve_thesemicolons/web/CardController.java
+++ src/main/java/org/example/andensemesterproeve_thesemicolons/web/CardController.java
@@ -21,64 +21,37 @@ public class CardController {
     private CardService cardService;
 
     @GetMapping("/myCards")
-    public String getMyCards(@RequestParam(required = false) String selectedSetSort, HttpSession session, Model model){
-
+    public String getMyCards(@RequestParam(required = false) String selectedSetSort,
+                             @RequestParam(required = false) String selectedTypeSort,
+                             HttpSession session, Model model){
 
         model.addAttribute("sets", cardService.getAllSets());
         model.addAttribute("types", cardService.getAllTypes());
         model.addAttribute("selectedSetSort", selectedSetSort);
+        model.addAttribute("selectedTypeSort", selectedTypeSort);
+        User sessionUser = (User) session.getAttribute("currentUser");
 
-        if (selectedSetSort == null) {
-            System.out.println("Er i selectedSetSort==null");
-            User sessionUser = (User) session.getAttribute("currentUser");
-            model.addAttribute("cards", sessionUser.getCards());
+        if (selectedSetSort != null && selectedTypeSort != null) {
+            if (selectedSetSort.equals("Intet filter") && selectedTypeSort.equals("Intet filter")) {
+                model.addAttribute("cards", sessionUser.getCards());
+                return "/cards/myCards";
+            }
+
+            if (selectedTypeSort.equals("Intet filter")) {
+                model.addAttribute("cards", cardService.filterUserCardsBySet(sessionUser, selectedSetSort));
+                return "/cards/myCards";
+            }
+
+            if (selectedSetSort.equals("Intet filter")) {
+                model.addAttribute("cards", cardService.filterUserCardsByType(sessionUser, selectedTypeSort));
+                return "/cards/myCards";
+            }
+
+            model.addAttribute("cards", cardService.filterUserCardsBySetAndType(sessionUser, selectedSetSort, selectedTypeSort));
             return "/cards/myCards";
         }
-        System.out.println("selectedSetSort = " + selectedSetSort);
 
-        if (selectedSetSort.equals("Intet filter")) {
-            System.out.println("Er i selectedSetSort.equals(intet filter)");
-            User sessionUser = (User) session.getAttribute("currentUser");
-            model.addAttribute("cards", sessionUser.getCards());
-            return "/cards/myCards";
-        }
-
-        /*
-        User sessionUser = (User) session.getAttribute("currentUser");
-        List<Card> testCards = sessionUser.getCards();
-        List<Card> randomCards = new ArrayList<>();
-        randomCards.add(testCards.get(1));
-        randomCards.add(testCards.get(2));
-        model.addAttribute("cards", testCards);
-        */
-
-        model.addAttribute("cards", new ArrayList<>());
-        return "/cards/myCards";
-
-
-    }
-
-/*
-    @GetMapping("/myCards")
-    public String getMyCards(HttpSession session, Model model){
-        User sessionUser = (User) session.getAttribute("currentUser");
         model.addAttribute("cards", sessionUser.getCards());
-        model.addAttribute("sets", cardService.getAllSets());
-        model.addAttribute("types", cardService.getAllTypes());
         return "/cards/myCards";
     }
-    */
-
-/*
-    @GetMapping("/myCards/set/{set}")
-    public String getMyCards(@PathVariable String set, HttpSession session, Model model){
-        System.out.println("Settet er: " + set);
-        User sessionUser = (User) session.getAttribute("currentUser");
-        model.addAttribute("cards", sessionUser.getCards());
-        model.addAttribute("sets", cardService.getAllSets());
-        model.addAttribute("types", cardService.getAllTypes());
-        return "/cards/myCards";
-    }
-    */
-
 }
