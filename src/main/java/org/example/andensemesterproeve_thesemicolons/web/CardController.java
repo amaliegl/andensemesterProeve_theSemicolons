88e4befdb9804f482b3e406d9b6bbc0c793b5d22
sara <saj016@edu.zealand.dk>
2026-05-18 09:25:39 +0200

    Sender nu brugers kort med til myCards-HTML'en

--- src/main/java/org/example/andensemesterproeve_thesemicolons/web/CardController.java
+++ src/main/java/org/example/andensemesterproeve_thesemicolons/web/CardController.java
@@ -1,13 +1,18 @@
 package org.example.andensemesterproeve_thesemicolons.web;
 
+import jakarta.servlet.http.HttpSession;
+import org.example.andensemesterproeve_thesemicolons.domain.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CardController {
 
     @GetMapping("/myCards")
-    public String getMyCards(){
+    public String getMyCards(HttpSession session, Model model){
+        User sessionUser = (User) session.getAttribute("currentUser");
+        model.addAttribute("cards", sessionUser.getCards());
         return "/cards/myCards";
     }
 }
