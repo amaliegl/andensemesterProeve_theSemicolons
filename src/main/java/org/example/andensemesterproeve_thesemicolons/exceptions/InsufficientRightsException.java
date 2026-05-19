851d7edcf39955fcd3fef59a03ba892c39a4371d
sara <saj016@edu.zealand.dk>
2026-05-19 11:25:11 +0200

    Ekstra admin-tjeks tilføjet

---
+++ src/main/java/org/example/andensemesterproeve_thesemicolons/exceptions/InsufficientRightsException.java
@@ -0,0 +1,7 @@
+package org.example.andensemesterproeve_thesemicolons.exceptions;
+
+public class InsufficientRightsException extends RuntimeException {
+    public InsufficientRightsException(String message) {
+        super(message);
+    }
+}
